135dfc1ae14d2a7019ccd08b2e6dec03877ea67a
Chinmay Vibhute <chinmayvib@gmail.com>
2019-03-09 05:44:21 +0530

    completed

---
+++ build/precache-manifest.6d0d1943e1bcd9a9aa962d630686f505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea840f97e0b58cf7aa85",
+    "url": "/phonebook/static/css/main.c667c5e3.chunk.css"
+  },
+  {
+    "revision": "ea840f97e0b58cf7aa85",
+    "url": "/phonebook/static/js/main.ea840f97.chunk.js"
+  },
+  {
+    "revision": "9688c312e3e901fff5d0",
+    "url": "/phonebook/static/js/runtime~main.9688c312.js"
+  },
+  {
+    "revision": "5f0dd84caf789070604f",
+    "url": "/phonebook/static/js/2.5f0dd84c.chunk.js"
+  },
+  {
+    "revision": "9782c75361e81a3743b4f78d18173ea2",
+    "url": "/phonebook/index.html"
+  }
+];
