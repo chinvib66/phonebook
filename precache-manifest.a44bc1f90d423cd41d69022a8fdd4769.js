39173cfc2f6d94f4bc036a01907b45b9d58d94d7
Chinmay Vibhute <chinmayvib@gmail.com>
2019-03-09 07:48:42 +0530

    Updates

--- precache-manifest.a44bc1f90d423cd41d69022a8fdd4769.js
+++ precache-manifest.a44bc1f90d423cd41d69022a8fdd4769.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ff4c777bd7f9cdb7e7bb",
+    "revision": "5296d222f1ac680059a2",
     "url": "/phonebook/static/css/main.c667c5e3.chunk.css"
   },
   {
-    "revision": "ff4c777bd7f9cdb7e7bb",
-    "url": "/phonebook/static/js/main.ff4c777b.chunk.js"
+    "revision": "5296d222f1ac680059a2",
+    "url": "/phonebook/static/js/main.5296d222.chunk.js"
   },
   {
     "revision": "9688c312e3e901fff5d0",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/phonebook/static/js/2.5f0dd84c.chunk.js"
   },
   {
-    "revision": "ffc41165523107327675536fc5855048",
+    "revision": "233a7c632f68771b53e3871831a50042",
     "url": "/phonebook/index.html"
   }
 ];
